29135459a6b829156361cde6d3bf4c085e1e742b
Daniel Amaya <damayalop@gmail.com>
2023-11-26 17:31:03 -0600

    feat: :sparkles: Update project structure

---
+++ src/data.js
@@ -0,0 +1,58 @@
+export const DATA = [
+  {
+    title: "Product card",
+    img: "./src/images/product-preview-card-component.png",
+    url: "./src/components/product-preview-card-component/index.html",
+    languages: {
+      js: false,
+    },
+  },
+  {
+    title: "Interactive card",
+    img: "./src/images/interactive-rating-component.png",
+    url: "./src/components/interactive-rating-component/index.html",
+    languages: {
+      js: true,
+    },
+  },
+  {
+    title: "NFT card",
+    img: "./src/images/nft-preview-card-component.png",
+    url: "./src/components/nft-preview-card-component/index.html",
+    languages: {
+      js: false,
+    },
+  },
+  {
+    title: "Order summary component",
+    img: "./src/images/order-summary-component.png",
+    url: "./src/components/order-summary-component/index.html",
+    languages: {
+      js: false,
+    },
+  },
+  {
+    title: "Three-column card",
+    img: "./src/images/column-preview-card-component.png",
+    url: "./src/components/column-preview-card-component/index.html",
+    languages: {
+      js: false,
+    },
+  },
+  {
+    title: "Stats card",
+    img: "./src/images/stats-preview-card-component.png",
+    url: "./src/components/stats-preview-card-component/index.html",
+    languages: {
+      js: false,
+    },
+  },
+  {
+    title: "Four card feature section",
+    img: "./src/images/four-card-feature-section.png",
+    url: "./src/components/four-card-feature-section/index.html",
+    languages: {
+      js: false,
+    },
+  },
+];
